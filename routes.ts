d320b1350d8adf169ca8f08f31fb808602af4755
Han Shen Ng <endeavorforconsummation@gmail.com>
2025-07-30 15:41:33 +0800

    feat - Integrate with React router & shadcn/ui

---
+++ routes.ts
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router";
+import HomePage from './src/ui/pages/HomePage/HomePage.tsx';
+import AboutPage from "./src/ui/pages/AboutPage/AboutPage.tsx";
+import Layout from "./src/ui/Layout.tsx";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        Component: Layout,
+        children: [
+            // Home page
+            { index: true, Component: HomePage },
+            { path: "about", Component: AboutPage },
+        ],
+    },
+]);
