57a043f50639c4a9c175d3ea078c81c1bdd6bfb0
Han Shen Ng <endeavorforconsummation@gmail.com>
2025-08-01 19:51:07 +0800

    feat - Home Page

--- src/ui/Layout.tsx
+++ src/ui/Layout.tsx
@@ -1,40 +1,10 @@
-import { Link, Outlet } from "react-router";
-import {
-    NavigationMenu,
-    NavigationMenuList,
-    NavigationMenuItem,
-    NavigationMenuLink,
-} from "@/components/ui/navigation-menu";
+import { Outlet } from "react-router";
 import "@/src/ui/App.css";
 
 function Layout() {
   return (
     <div className="flex flex-col justify-between w-screen h-screen">
-      <header>
-        <NavigationMenu viewport={false}>
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuLink>
-                <Link to="/">Home</Link>
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuLink>
-                <Link to="/about">About</Link>
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
-      </header>
-      <main className="flex-grow">
-        <Outlet />
-      </main>
-      <footer>
-        <p>
-          Copyright &copy; 2025 CERC Wedding Liturgy Generator. All rights
-          reserved.
-        </p>
-      </footer>
+      <Outlet />
     </div>
   );
 }
